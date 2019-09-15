2b228fb8f45619d649fc5c705c848012642e343a
Tekisen <55293966+Tekisen@users.noreply.github.com>
2019-09-14 20:57:08 -0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+let n = 123;                  // Data type Numeric
+"str" = n                     // Data type String
+Console.log(- (10-2))         // Example of Urnary Operator
+Console.log(3<2)              // Data Type Boolean False
+Console.log(3>2)              // Data Type Boolean True
+Console.log(8 * null)
+Console.log("five" + 2)
+Console.log(false == 0)
